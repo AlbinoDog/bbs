1ab25af605c1e0223236fe5cf4cb51862f5b1aeb
AlbinoDog <sugiyamata@indival.co.jp>
2016-07-19 14:54:09 +0900

    bbs-html

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
-<?php
-
-$number = rand();
-if ($number % 2 == 0){
-    echo $number."は偶数です"."<br/>";
-}else {
-    echo $number."は奇数です"."<br/>";
-}
-
-$fruits = array ('apple','orange','lemmon');
-echo $fruits[0]."<br>";
-
-$users[] = 'sugiyama'."<br>";
-$users[] = 'chefan'."<br>";
-echo $users[1];
-
-$userdata = array(
-    'name' => 'sugiyama',
-    'age' => '29',
-    'gender' => 'male',
-    'job' => 'engineer',
-);
-
-echo $userdata['age'];
+<!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="utf-8"/>
+        <title>ひとこと掲示板</title>
+    </head>
+    <body>
+        <h1>ひとこと掲示板</h1>
+        <form action="bbs.php" method="post">
+            名前:<input tpe="text" name="name" /> <br />
+            ひとこと:<input type="text" name="comment" size="60" /><br />
+        </form>
+    </body>
+    </html>
